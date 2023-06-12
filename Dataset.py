dde813b6a8e7547de6dbb609343778f60111b575
chaitanyabirla <118895026+chaitanyabirla@users.noreply.github.com>
2023-06-12 18:05:59 +0530

    h

--- Dataset.py
+++ Dataset.py
@@ -1,26 +1,5 @@
-import random
+import pandas
 import csv
-
-#def generate_student_id():
- #   return range(100, 999)
-
-#student_ids = [generate_student_id() for i in range(500)]
-
-#with open('student_ids.csv', mode='w', newline='') as file:
-#    writer = csv.writer(file)
-#    writer.writerow(['Student ID'])
-#    for id in student_ids:
-#        writer.writerow([id])
-
-
-#def generate_teacher_id():
-#    x = random.randint(2201,2250)
-#    y=''
-#    if x<10:
-#        y = "0" + str(x)
-#    else:
-#        y = str(x)
-#    return y
 
 for i in range(2201,2250):
     teacher_ids=[].append 
