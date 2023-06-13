2952dbe1c53988dd965246c326255eaedd6ec45e
chaitanyabirla <118895026+chaitanyabirla@users.noreply.github.com>
2023-06-13 19:59:46 +0530

    h

--- Main Body.py
+++ Main Body.py
@@ -7,14 +7,31 @@ print()
 
 print("User ID  guide\nFor student: 'STUD' followed by Registration Number (STUD2345678)\nFor Internship Guide: 'IG' followed by Employee ID (IG2345)\nFor Admin: 'A' followed by Employee ID (A2345)")
 user = input("Enter your User ID: ")
+status = True
+
 if re.match(r"^(STUD)", user):
-    login.student_login(user[4:])
-    user_is = "S"
+    while status == True:
+        if login.student_login(user[4:]):
+            registration() 
+            status = False
+        else:
+            status = True
+    
 elif re.match(r"^(IG)", user):
-    login.guide_login(user[2:])
-    user_is = "IG"
+    while status == True:
+        if login.guide_login(user[2:]):
+            registration() 
+            status = False
+        else:
+            status = True
+    
 elif re.match(r"^(A)", user):
-    login.admin_login(user[2:])
-    user_is = "A"
+    while status == True:
+        if login.admin_login(user[2:]):
+            registration() 
+            status = False
+        else:
+            status = True
+    
 else:
     print("Enter a vaild input")
