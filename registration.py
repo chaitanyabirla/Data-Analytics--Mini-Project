872c28cd0b7edb3472adc537a9feba59c2b807ec
kanishksaleria <118894895+kanishksaleria@users.noreply.github.com>
2023-06-16 13:47:11 +0530

    student reg done

--- registration.py
+++ registration.py
@@ -111,7 +111,7 @@ def student_registration():
     if not flag:
         print("Invalid registration number.")
 
-    database = pd.DataFrame(datalist, columns=['User ID', 'Password','Name', 'Course Name', 'Year', 'Course',"Department"])
+    database = pd.DataFrame(datalist, columns=['User ID', 'Password','Name',"Department", 'Course Name', 'Year', 'Course'])
     return database,datalist
 
 
@@ -172,4 +172,77 @@ def internship():
         print("Invalid registration number.")
     intern_data= pd.DataFrame(intern_detail_list, columns=['User ID',"Company" , "Domain" , "Weeks of Work" , "Guide Name" , "Guide Email" , "Guide Designation"])
     return intern_data
-print(internship())
+#print(internship())
+
+
+
+def coursework():
+    from datetime import datetime
+    course_detail_list=[]
+    for i in range(2211200,2211700):
+        j=[i,NaN,NaN,NaN,NaN,NaN,NaN]
+        course_detail_list.append(j)
+    
+    flag=False
+    regnum=int(input("enter your registration number"))
+    for i in range(len(course_detail_list)):
+        if course_detail_list[i][0] == regnum:
+            course_name = input("Enter the course name: ")
+            domain = input("Enter the domain of the course: ")
+            start = input("Enter start date in YYYY-MM-DD format: ")
+            start_date=datetime.strptime(start, "%Y-%m-%d")
+            end = input("Enter end date in YYYY-MM-DD format: ")
+            end_date=datetime.strptime(end, "%Y-%m-%d")
+            guide_email = input("Enter the guide's email ID: ")
+            guide_designation = input("Enter the guide's designation:")
+            
+            course_detail_list[i][1]=course_name
+            course_detail_list[i][2]=domain
+            course_detail_list[i][3]=start_date
+            course_detail_list[i][4]=end_date
+            course_detail_list[i][5]=guide_email
+            course_detail_list[i][6]=guide_designation
+            flag=True
+            break
+    if not flag:
+        print("Invalid registration number.")
+    course_data= pd.DataFrame(course_detail_list, columns=['User ID',"Course" , "Domain" , "Start Date" , "End Date" , "Guide Email" , "Guide Designation"])
+    return course_data
+#print(coursework())
+
+
+def Reserchwork():
+    from datetime import datetime
+    reserch_detail_list=[]
+    for i in range(2211200,2211700):
+        j=[i,NaN,NaN,NaN,NaN,NaN,NaN,NaN]
+        reserch_detail_list.append(j)
+    
+    flag=False
+    regnum=int(input("enter your registration number"))
+    for i in range(len(reserch_detail_list)):
+        if reserch_detail_list[i][0] == regnum:
+            university = input("Enter the university you are doing reserch under: ")
+            domain = input("Enter the domain of the reserch: ")
+            start = input("Enter start date in YYYY-MM-DD format: ")
+            start_date=datetime.strptime(start, "%Y-%m-%d")
+            end = input("Enter end date in YYYY-MM-DD format: ")
+            end_date=datetime.strptime(end, "%Y-%m-%d")
+            guide_email = input("Enter the guide's email ID: ")
+            guide_designation = input("Enter the guide's designation:")
+            publication=input("Enter the name of publication your reserch is getting published under:")
+            
+            reserch_detail_list[i][1]=university
+            reserch_detail_list[i][2]=domain
+            reserch_detail_list[i][3]=start_date
+            reserch_detail_list[i][4]=end_date
+            reserch_detail_list[i][5]=guide_email
+            reserch_detail_list[i][6]=guide_designation
+            reserch_detail_list[i][7]=publication
+            flag=True
+            break
+    if not flag:
+        print("Invalid registration number.")
+    course_data= pd.DataFrame(reserch_detail_list, columns=['User ID',"University" , "Domain" , "Start Date" , "End Date" , "Guide Email" , "Guide Designation","Publication"])
+    return course_data
+print(Reserchwork())
